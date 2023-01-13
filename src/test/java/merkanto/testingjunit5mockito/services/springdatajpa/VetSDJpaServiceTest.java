e0aae4b346dfc4e684efb435f0202096c41df00d
Merkanto <miroslav.i.zhelezchev@gmail.com>
2023-01-13 17:53:16 +0200

    Adding Mockito Tests for Vet Service.

---
+++ src/test/java/merkanto/testingjunit5mockito/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,29 @@
+package merkanto.testingjunit5mockito.services.springdatajpa;
+
+import merkanto.testingjunit5mockito.repositories.VetRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    VetRepository vetRepository;
+
+    @InjectMocks
+    VetSDJpaService vetSDJpaService;
+
+    @Test
+    void deleteById() {
+
+        vetSDJpaService.deleteById(1L);
+        verify(vetRepository).deleteById(1L);
+    }
+}
