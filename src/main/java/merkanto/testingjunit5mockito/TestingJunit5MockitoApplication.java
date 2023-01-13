5f783bd20ea4bfe56074bf600885e94cfd260ba8
Merkanto <miroslav.i.zhelezchev@gmail.com>
2023-01-13 16:05:02 +0200

    Init commit.

---
+++ src/main/java/merkanto/testingjunit5mockito/TestingJunit5MockitoApplication.java
@@ -0,0 +1,13 @@
+package merkanto.testingjunit5mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingJunit5MockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingJunit5MockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/merkanto/testingjunit5mockito/TestingJunit5MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package merkanto.testingjunit5mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingJunit5MockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
